aa807c99b1956625da51e64df0fc32330426daba
SigridZhangSY <sigridzhang0603@163.com>
2016-07-22 18:05:50 +0800

    fix endpoint in verify.js

--- verify/verify.js
+++ verify/verify.js
@@ -7,7 +7,8 @@ var request = require('request');
 var chai = require('chai');
 var assert = chai.assert;
 var tv4 = require('tv4');
-var endpoint = "http://localhost:8888";
+// var endpoint = "http://localhost:8888";
+var endpoint = process.env.ENDPOINT;
 
 console.log(endpoint);
 
